41ed55fba626eda3431815e33b7e87125fe3326b
Boki <bojan.sejnjanovic@gmail.com>
2026-03-28 13:44:37 +0100

    add linter and formater

--- src/objects.js
+++ src/objects.js
@@ -1,53 +1,57 @@
-import { formatDistance, isSameDay, isAfter } from "date-fns"
+import { formatDistance, isSameDay, isAfter } from "date-fns";
 
 export class ToDoTask {
-    completed = false
-    createdDate = new Date()
-    id = crypto.randomUUID()
+  completed = false;
+  createdDate = new Date();
+  id = crypto.randomUUID();
 
-    constructor(title, desc="", dueDate, priority="normal") {
-        this.title = title
-        this.desc = desc
-        this.dueDate = new Date(dueDate)
-        this.priority = priority
+  constructor(title, desc = "", dueDate, priority = "normal") {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = new Date(dueDate);
+    this.priority = priority;
+  }
+
+  dueIn() {
+    const now = new Date();
+
+    if (isSameDay(this.dueDate, now) || isAfter(this.dueDate, now)) {
+      const distance = formatDistance(this.dueDate, now);
+      return `Due in ${distance}`;
+    } else {
+      return "Due time has passed";
     }
+  }
 
-    dueIn() {
-        const now = new Date()
+  static fromStored(data) {
+    const storedTask = new this(
+      data.title,
+      data.desc,
+      data.dueDate,
+      data.priority,
+    );
 
-        if (isSameDay(this.dueDate, now) || isAfter(this.dueDate, now)) {
-            const distance = formatDistance(this.dueDate, now)
-            return `Due in ${distance}`
-        } else {
-            return "Due time has passed"
-        }
-    }
+    storedTask.completed = data.completed;
+    storedTask.createdDate = data.createdDate;
+    storedTask.id = data.id;
 
-    static fromStored(data) {
-
-        const storedTask = new this(data.title, data.desc, data.dueDate, data.priority)
-
-        storedTask.completed = data.completed
-        storedTask.createdDate = data.createdDate
-        storedTask.id = data.id
-
-        return storedTask
-    }
+    return storedTask;
+  }
 }
 
 export class Project {
-    listTasks = []
+  listTasks = [];
 
-    constructor(name) {
-        this.name = name
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    add(task) {
-        this.listTasks.push(task)
-    }
+  add(task) {
+    this.listTasks.push(task);
+  }
 
-    remove(task) {
-        const idx = this.listTasks.findIndex(el => el.id === task.id)
-        this.listTasks.splice(idx, 1)
-    }
+  remove(task) {
+    const idx = this.listTasks.findIndex((el) => el.id === task.id);
+    this.listTasks.splice(idx, 1);
+  }
 }
